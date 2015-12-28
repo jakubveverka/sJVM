5a82c061e334a71b2b120130a5c351fd0b9e2b41
Jakub Veverka <veverkajakub@gmail.com>
2015-12-28 19:27:40 +0100

    SAT solver update, added brackets support for int array solution to 1 depth

--- SAT/src/SatIntInput.java
+++ SAT/src/SatIntInput.java
@@ -20,49 +20,50 @@ public class SatIntInput {
         //int elementsCount = 2;
         //int[] formula = {0,-2,-3,0,-2,1};
         int elementsCount = 2;
-        int[][] formula = {{-3}, {0},{-1},{0},{-2},{1}};
+        int[] formula = {-3, 0,-1,0,-2,1};
         //int elementsCount = 2;
         //int[] formula = {-3, 0,-1,0,-2,1};
-        int[] results = new int[formula.length];
+        int prevResult = -1;
         int isTrue = 0;
 
         int countOfPossibleCombinations = 1;
         for(int i = 0; i < elementsCount; i++) countOfPossibleCombinations *= 2;
 
         for(int combination = 0; combination < countOfPossibleCombinations; combination++) {
-            System.out.println("Testing combination " + Integer.toBinaryString(combination));
+            //System.out.println("Testing combination " + Integer.toBinaryString(combination));
+            prevResult = -1;
             for(int i = 0; i < formula.length-1; i+=2) {
                 int moduloAdd = 0;
                 int partResult;
                 int nextResult;
-                if(i == 0) {
-                    if(formula[i][0] == -3) {
-                        partResult = (getElementOnIPosition(formula[i+1][0], combination)+1) % 2;
+                if(prevResult == -1) {
+                    if(formula[i] == -3) {
+                        partResult = (getElementOnIPosition(formula[i+1], combination)+1) % 2;
                         i += 1;
                     } else {
-                        partResult = getElementOnIPosition(formula[i][0], combination);
+                        partResult = getElementOnIPosition(formula[i], combination);
                     }
                 } else {
-                    partResult = results[i];
+                    partResult = prevResult;
                 }
-                if(formula[i+2][0] == -3) {
+                if(formula[i+2] == -3) {
                     moduloAdd = 1;
                 }
-                nextResult = (getElementOnIPosition(formula[i+2+moduloAdd][0], combination) + moduloAdd)%2;
-                if(formula[i+1][0] == -1) {
-                    if(partResult == 1 || nextResult == 1) results[i+moduloAdd+2] = 1;
-                    else results[i+moduloAdd+2] = 0;
-                } else if(formula[i+1][0] == -2) {
-                    if(partResult == 1 && nextResult == 1) results[i+moduloAdd+2] = 1;
-                    else results[i+moduloAdd+2] = 0;
+                nextResult = (getElementOnIPosition(formula[i+2+moduloAdd], combination) + moduloAdd)%2;
+                if(formula[i+1] == -1) {
+                    if(partResult == 1 || nextResult == 1) prevResult = 1;
+                    else prevResult = 0;
+                } else if(formula[i+1] == -2) {
+                    if(partResult == 1 && nextResult == 1) prevResult = 1;
+                    else prevResult = 0;
                 }
                 i += moduloAdd;
             }
-            if(results[results.length-1] == 1) {
-                System.out.println("----> SAT splnitelny pro " + Integer.toBinaryString(combination));
+            if(prevResult == 1) {
+                //System.out.println("----> SAT splnitelny pro " + Integer.toBinaryString(combination));
                 isTrue = 1;
             }
         }
-        if(isTrue != 1) System.out.println("SAT nesplnitelny.");
+        //if(isTrue == 1) System.out.println("SAT nesplnitelny.");
     }
 }
